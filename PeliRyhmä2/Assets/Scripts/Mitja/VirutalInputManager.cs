168b2afa24d88ba40c4a69825aaabf7662318f84
Mitja Pimenov <mitja.pimenov@gmail.com>
2022-06-02 20:09:44 +0300

    Messing around in my scene

---
+++ PeliRyhmä2/Assets/Scripts/Mitja/VirutalInputManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameProject
+{
+public class VirutalInputManager : Singleton<VirutalInputManager>
+{
+    public bool MoveRight;
+    public bool MoveLeft;
+}
+
+}
