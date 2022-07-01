38d1f4ea58292bf1e133478019527bb4d0633960
heikkinenlaura <74296164+mitjapimenov@users.noreply.github.com>
2022-07-01 15:21:51 +0300

    CP system
    pientä duunailua omassa scenssä

---
+++ PeliRyhmä2/Assets/Scripts/Mitja/Checkpoint.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    public GameObject Sphere;
+    Vector3 spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y < -20f)
+        {
+            gameObject.transform.position = spawnPoint;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("CheckPoint"))
+        {
+            spawnPoint = Sphere.transform.position;
+            Debug.Log("CheckPoint");
+            Destroy(Sphere);
+        }
+    }
+}
